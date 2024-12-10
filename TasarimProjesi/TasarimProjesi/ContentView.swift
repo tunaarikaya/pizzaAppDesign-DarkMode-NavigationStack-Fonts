52074ac2d7da10d5872b36d79d32b5a13294bd91
Mehmet Tuna Arıkaya <m.tunaarikaya@gmail.com>
2024-12-10 20:48:13 +0300

    a
    a

---
+++ TasarimProjesi/TasarimProjesi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TasarimProjesi
+//
+//  Created by Mehmet Tuna Arıkaya on 9.12.2024.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+    }
+    
+    #Preview {
+        ContentView()
+        
+    }
+}
